480fc2b466dad480674a3b332fe1f304b22918ad
Andrew Faircloth <andrewkfaircloth@gmail.com>
2016-04-26 17:28:01 -0400

    adjusted app.js config to deploy

--- src/app.js
+++ src/app.js
@@ -13,8 +13,9 @@ var router = require('./api');
 // create an instance to allow for middleware we create
 var app = express();
 
-// require cool for heroku example
-var cool = require('cool-ascii-faces');
+// http
+var http = require('http');
+var port = process.env.PORT || 5000);
 
 // require database - mongoose is a singleton, when you do it in one file, changes happen across the node process
 require('./database');
@@ -30,22 +31,20 @@ app.use(parser.json());
 
 // auto does the namespacing for new routes
 app.use('/api', router);
-
-// set port
-app.set('port', (process.env.PORT || 5000));
+  
+// create server
+http.createServer( (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World');
+}).listen(port, () => {
+  console.log("Server running at " + port);
+});
 
 // fires up the server in the browser
 // app.listen(port, function() {
 //   console.log('the server is running on port ' + port);
 // });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
 
 
