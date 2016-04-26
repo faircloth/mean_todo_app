c6aba57c6083ce969fe33dbb18c4b33419497a1d
Andrew Faircloth <andrewkfaircloth@gmail.com>
2016-04-26 17:10:41 -0400

    heroku set up for nodejs

--- src/app.js
+++ src/app.js
@@ -13,19 +13,8 @@ var router = require('./api');
 // create an instance to allow for middleware we create
 var app = express();
 
-const http = require('http');
-
-// th video on mongo set up for heroku
-const port = process.env.PORT || 3000;
-
-http.createServer();
-
-//   () => {
-//   console.log('create server');
-// });
-// .listen(port, () => {
-//   console.log('listen on create server');
-// });
+// require cool for heroku example
+var cool = require('cool-ascii-faces');
 
 // require database - mongoose is a singleton, when you do it in one file, changes happen across the node process
 require('./database');
@@ -42,9 +31,20 @@ app.use(parser.json());
 // auto does the namespacing for new routes
 app.use('/api', router);
 
+// set port
+app.set('port', (process.env.PORT || 5000));
+
 // fires up the server in the browser
-app.listen(port, function() {
-  console.log('the server is running on port' + port);
+// app.listen(port, function() {
+//   console.log('the server is running on port ' + port);
+// });
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
